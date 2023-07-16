bb488b86dddb27a846fdf06fc514fb3350de0d27
ayoniyi <ayo.a.niyi@gmail.com>
2023-07-15 23:20:13 -0700

    w/o dots

---
+++ main.js
@@ -0,0 +1,17 @@
+var tl = gsap.timeline();
+
+tl.from(".dots1", {
+  duration: 2.5,
+  //opacity: 0,
+  y: [-40, 0],
+  repeat: -1,
+});
+// tl.from(".bracket", {
+//   duration: 0.3,
+//   scale: 0,
+//   margin: 0,
+// });
+// tl.from("#loader", {
+//   duration: 0.2,
+//   scale: 0,
+// });
