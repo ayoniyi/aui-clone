c08c2eeb1398e051bce3e9be6d5e8ad02e45a979
ayoniyi <ayo.a.niyi@gmail.com>
2023-07-16 16:54:15 -0700

    fix inputs

--- main.js
+++ main.js
@@ -1,17 +1,36 @@
-var tl = gsap.timeline();
+const form = document.getElementById("form");
+const username = document.getElementById("name");
+const email = document.getElementById("email");
+const formBtn = document.getElementById("formBtn");
 
-tl.from(".dots1", {
-  duration: 2.5,
-  //opacity: 0,
-  y: [-40, 0],
-  repeat: -1,
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
 });
-// tl.from(".bracket", {
-//   duration: 0.3,
-//   scale: 0,
-//   margin: 0,
+// form.addEventListener("change", () => {
+//   alert("typing");
+//   checkInputs();
 // });
-// tl.from("#loader", {
-//   duration: 0.2,
-//   scale: 0,
-// });
+username.addEventListener("change", () => {
+  checkInputs();
+});
+email.addEventListener("change", () => {
+  checkInputs();
+});
+
+// const isValidEmail = email => {
+//     const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+//     return re.test(String(email).toLowerCase());
+// }
+
+const checkInputs = () => {
+  const usernameValue = username.value.trim();
+  const emailValue = email.value.trim();
+
+  if (usernameValue !== "" && emailValue !== "") {
+    formBtn.classList.add("activeBtn");
+    // formBtn.classList.remove()
+  } else {
+    formBtn.classList.remove("activeBtn");
+    // formBtn.classList.add("")
+  }
+};
